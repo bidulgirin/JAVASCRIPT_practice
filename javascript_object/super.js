a3f056c2d0757ffa70bf512b3e61812d13a09621
bidulgirin <de05164@naver.com>
2022-07-06 12:32:03 +0900

    super

--- javascript_object/super.js
+++ javascript_object/super.js
@@ -14,18 +14,38 @@ class Person{
     }
     // 보통 class 안쪽에 쓰는게 권장됨
     sum(){
-        return 'this : '+ (this.first + this.second);
+        return this.first + this.second;
     }
 }
 
 class PersonPlus extends Person{
 
+    constructor(name, first, second, third){ 
 
-        avg(){
-            return (this.first + this.second)/2;
-        }
+        super(name, first, second); //부모에 있는 코드를 받아온다
+
+         this.third = third;
+ 
+     }
+
+    sum(){
+        return super.sum() + this.third;
+    }
+
+    avg(){
+        return (this.first + this.second + this.third)/3;
+    }
 
 }
-var kim = new PersonPlus('kim' ,10, 20);
-console.log('kim', kim);
+var kim = new PersonPlus('kim' ,10, 20, 30);
+var lee = new PersonPlus('lee' ,10, 10, 10);
 
+console.log("kim.sum()", kim.sum());
+console.log("kim.sum()", kim.avg());
+
+//정리
+/*
+    super의 두가지 용법
+    1. super(): 부모-class의 생성자(constructor)를 참조한다.
+    2. super.method() : 부모-class의 prototype-method를 참조한다.
+*/
